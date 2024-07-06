aba46e25da2e25784bc03cd75492a311dd68cf43
Noémie Tapie <noemie.tapie@gmail.com>
2024-07-06 15:01:38 +0300

    zoom in the pictures

--- Main/js/scripts.js
+++ Main/js/scripts.js
@@ -63,3 +63,35 @@ expandBtns.forEach(btn => {
 });
 
 
+function openModal(imageSrc) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "flex";
+    modalImg.src = imageSrc;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    const closeBtn = document.getElementsByClassName("close")[0];
+
+    const images = document.getElementsByClassName("img-portfolio");
+    for (let img of images) {
+        img.addEventListener("click", function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        });
+    }
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    modal.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+
