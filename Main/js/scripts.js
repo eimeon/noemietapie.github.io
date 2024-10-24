d01312d56295782ee839bf15560fc316ed1afbd5
Noémie Tapie <noemie.tapie@gmail.com>
2024-10-24 19:38:15 +0300

    projects menu updated

--- Main/js/scripts.js
+++ Main/js/scripts.js
@@ -88,4 +88,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+function toggleContent(contentId) {
+    var content = document.getElementById(contentId);
+    content.classList.toggle('active');
+  }
+
 
